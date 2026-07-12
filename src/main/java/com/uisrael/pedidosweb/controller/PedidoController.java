03abcbdd1c06ab3d7a1983e95f96ca65ef8f9d50
e1726309956-cloud <e1726309956@uisrael.edu.ec>
2026-07-12 18:40:21 -0500

    Primer commit

---
+++ src/main/java/com/uisrael/pedidosweb/controller/PedidoController.java
@@ -0,0 +1,14 @@
+package com.uisrael.pedidosweb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/pedido") //url
+public class PedidoController {
+	@GetMapping
+	public String leerpagina() {
+		return "/pedidos/listarpedidos";//ruta fisica de la paguina 
+	}
+}
---
+++ src/test/java/com/uisrael/pedidosweb/PedidoswebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uisrael.pedidosweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidoswebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
